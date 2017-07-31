6452626fff924431cf30e601aeba7160397dd867
Alan Edel <my.magic@mail.ru>
2017-07-31 17:44:20 +0800

    added 2 tabs menu

---
+++ ios/ExifTool/ExifTool.h
@@ -0,0 +1,13 @@
+//
+//  ExifTool.h
+//  ExifTool
+//
+//  Created by Alan on 31/07/2017.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExifTool : NSObject
+
+@end
